35092b7c5a5472bffe6daebbd80dc372c6fc46a3
Sunshine <guyang_developer@163.com>
2022-06-15 18:54:49 +0800

    add copyright

--- apollo-client/src/main/java/com/ctrip/framework/apollo/spring/boot/ApolloAutoConfiguration.java
+++ apollo-client/src/main/java/com/ctrip/framework/apollo/spring/boot/ApolloAutoConfiguration.java
@@ -20,7 +20,6 @@ import com.ctrip.framework.apollo.spring.config.ConfigPropertySourcesProcessor;
 import com.ctrip.framework.apollo.spring.config.PropertySourcesConstants;
 import com.ctrip.framework.apollo.spring.config.PropertySourcesProcessor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -31,7 +30,6 @@ import org.springframework.context.annotation.Configuration;
 public class ApolloAutoConfiguration {
 
   @Bean
-  @ConditionalOnMissingClass("org.springframework.cloud.context.scope.refresh.RefreshScope")
   public ConfigPropertySourcesProcessor configPropertySourcesProcessor() {
     return new ConfigPropertySourcesProcessor();
   }
